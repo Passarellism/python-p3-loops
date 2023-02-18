327087a28b1d1426e5e9801c8180f52a35bfaf8e
steve passarelli <passarellism@gmail.com>
2023-02-18 11:59:08 -0700

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    for i in range(10, 0, -1):
-        print(i)
-    
-    print("Happy New Year!")
+    countdown = 10
+    while countdown > 0:
+        print (countdown)
+        countdown = countdown - 1
+    print ("Happy New Year!")
 
 def square_integers(int_list):
-    return [i ** 2 for i in int_list]
+    return [ i ** 2 for i in int_list]
 
 def fizzbuzz():
     for i in range(1, 101):
